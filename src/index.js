66586f9be123058f70e4cc274f5904f506f47453
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2021-06-18 12:50:46 +0800

    move required file to top

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
+const imgScraper = require('./img-scraper')
+const imgDownloader = require('./img-dl')
+const descScraper = require('./desc-scraper')
+
 ;(async () => {
   console.log('# STARTING\n')
 
   console.log('# 1. Scrap Pokemon image URLs\n')
-
-  const imgScraper = require('./img-scraper')
   await imgScraper()
 
   console.log('\n# 2. Download Pokemon images\n')
-
-  const imgDownloader = require('./img-dl')
   await imgDownloader()
 
   console.log('\n# 3. Scrap Pokemon descriptions\n')
-
-  const descScraper = require('./desc-scraper')
   await descScraper()
 
   console.log('\n# DONE')
